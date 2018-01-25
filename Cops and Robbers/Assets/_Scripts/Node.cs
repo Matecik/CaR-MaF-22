1969d0ff0b3408949d6f91120e3e4b695a9e390b
Matecik <maximum.rich.1236@gmail.com>
2018-01-25 11:06:45 +0800

    Game can currently connect nodes

---
+++ Cops and Robbers/Assets/_Scripts/Node.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Node : NetworkBehaviour {
+
+	public List<Node> nodeConnections = new List<Node>();
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Cops and Robbers/Assets/_Scripts/NodeConnection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(LineRenderer))]
+
+public class NodeConnection : NetworkBehaviour {
+
+	//The two nodes that should be connected
+	public Node[] nodesBeingConnected = new Node[2];
+
+	LineRenderer lr;
+	Vector3[] positions = new Vector3[2];
+
+	void Start () {
+
+		//Store line renderer in lr
+		lr = GetComponent<LineRenderer>();
+		//Get the positions of both the nodes
+		positions[0] = nodesBeingConnected[0].transform.position;
+		positions[1] = nodesBeingConnected[1].transform.position;
+
+		//Tell the two nodes being connected that they are connected to each other
+		nodesBeingConnected [0].nodeConnections.Add (nodesBeingConnected [1]);
+		nodesBeingConnected [1].nodeConnections.Add (nodesBeingConnected [0]);
+
+		//Set the line renderer to the the correct positions and turn it on
+		lr.SetPositions(positions);
+		lr.enabled = true;
+	}
+
+}
